0277adc92b18486b7f1891a8242ac8fba8ffddba
Anders Sjberg <shoobah@gmail.com>
2014-12-23 15:13:06 +0100

    Slider m.m.

--- salen/main_2.js
+++ salen/main_2.js
@@ -1,38 +1,79 @@
 $(function() {
     "use strict";
-    var pixelProj = new ol.proj.Projection({
+    var settings_list = [{
+            name: 'Sälen',
+            extent: [0, 0, 50176, 30208],
+            layers: [{
+                name: 'Sälen 1956',
+                url: 'salen_1956/{z}/{x}/{-y}.jpg'
+            }, {
+                name: 'Sälen 2011',
+                url: 'salen_2011/{z}/{x}/{-y}.jpg'
+            }
+            // ,{
+            //     name: 'Karta',
+            //     url: 'tkarta/{z}/{x}/{-y}.jpg'
+            // }
+            ]
+        }],
+        settings = settings_list[0],
+        pixelProj = new ol.proj.Projection({
             code: 'pixel',
             units: 'pixels',
-            extent: [0, 0, 50176, 30208]
+            extent: settings.extent
         }),
 
-        layer1 = new ol.layer.Tile({
-            source: new ol.source.XYZ({
-                url: 'salen_1956/{z}/{x}/{-y}.jpg',
-                wrapX: false,
-                projection: pixelProj
+        layers = [];
+
+    for (var i = 0; i < settings.layers.length; i += 1) {
+        layers.push(
+            new ol.layer.Tile({
+                source: new ol.source.XYZ({
+                    url: settings.layers[i].url,
+                    wrapX: false,
+                    projection: pixelProj
+                })
             })
-        }),
-        layer2 = new ol.layer.Tile({
-            source: new ol.source.XYZ({
-                url: 'salen_2011/{z}/{x}/{-y}.jpg',
-                wrapX: false,
-                projection: pixelProj
-            })
-        }),
-        view = new ol.View({
-            center: [25088, 15104],
-            // extent: pixelProj.getExtent(),
-            zoom: 3,
-            minZoom: 2,
+        );
+    }
+    var view = new ol.View({
+            center: [25088, -15104],
+            //extent: [-30000, -10000, 60000, 30000],
+            zoom: 4,
+            minZoom: 1,
             maxZoom: 8,
             projection: pixelProj
         }),
+        layer_b = layers[1],
 
         map = new ol.Map({
-            layers: [layer1, layer2],
+            layers: layers,
             target: 'map',
             units: 'm',
-            view: view
-        });
+            view: view,
+            controls: []
+        }),
+        swipe = document.getElementById('swipe');
+
+    map.addControl(new ol.control.ZoomSlider());
+    map.addControl(new ol.control.ZoomToExtent());
+
+    layer_b.on('precompose', function(event) {
+        var ctx = event.context;
+        var width = ctx.canvas.width * (swipe.value / 100);
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.rect(width, 0, ctx.canvas.width - width, ctx.canvas.height);
+        ctx.clip();
+    });
+
+    layer_b.on('postcompose', function(event) {
+        var ctx = event.context;
+        ctx.restore();
+    });
+
+    swipe.addEventListener('input', function() {
+        map.render();
+    }, false);
 });
